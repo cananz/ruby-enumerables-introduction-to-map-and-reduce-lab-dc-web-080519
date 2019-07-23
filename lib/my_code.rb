95e294fe4e3dd64adddab4a279e0fc68f1733ea1
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-23 23:02:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,8 +47,11 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-
+  i = 0
+    while i < source_array.length do
+      +=
 end
+
 def reduce_to_any_true(source_array)
   
 end
