3e34ce243f251fc2460768fedf7d9e4eb9efec5f
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-23 22:44:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,17 @@
 # My Code here....
 
+#map-like
 def map_to_negativize(source_array)
-map_to_no_change(source_array)
-map_to_double(source_array)
-map_to_square(source_array)
+
+end
+def map_to_no_change(source_array)
+  
+end
+def map_to_double(source_array)
+  
+end
+def map_to_square(source_array)
+  
+end
+
+#reduce-like
